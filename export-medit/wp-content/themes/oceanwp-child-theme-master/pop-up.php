425be89a013c66aa7546ee578bbb535496f5dd5a
kahina2110 <kahina.aouadi@epitech.eu>
2024-03-12 11:58:46 +0100

    step 5 done

---
+++ export-medit/wp-content/themes/oceanwp-child-theme-master/pop-up.php
@@ -0,0 +1,41 @@
+<?php
+// On récupère les champs ACF nécessaires
+$titre=get_field('titre', 161);
+$description=get_field('description', 161);
+$lieu=get_field('lieu', 161);
+$date=get_field('date', 161);
+$lien=get_field('lien_google_maps', 161);
+if($lien && isset($lien['url'])){
+	$url = $lien['url'];
+}
+?>
+
+<div class="popup-overlay">
+	<div class="popup-salon">
+		<div class="popup-header">
+			<h3><?php echo $titre; ?> </h3>
+			<span class="popup-close"><i class="fa fa-times"></i></span>
+		</div>
+		<?php echo $description; ?>
+		<div class="popup-details">
+			<div class="popup-address">
+				<p><b>Le lieu</b></p>
+				<?php echo $titre; ?>
+				<a class="popup-link" href="<?php echo esc_url($url)?>" target="_blank">Voir sur Google Maps</a>
+			</div>
+			<div class="popup-address">
+				<p><b>La date</b></p>
+				<?php echo $date; ?>
+			</div>
+		</div>
+		<p class="popup-informations">Vous souhaitez plus d'informations concernant cet événement ?</p>
+		<?php
+		// On insère le formulaire de demandes de renseignements
+		 echo do_shortcode('[contact-form-7 id="910" title="Formulaire salon New York"]');
+		?>
+	</div>
+</div>
+
+
+
+	
